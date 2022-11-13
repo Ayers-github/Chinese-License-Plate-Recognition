ea397219dd090f336e18b7af6f9e5280b2d038bd
Ayers-github <1419031719@qq.com>
2022-11-13 16:36:32 +0800

    更新优化代码

--- ncnn_demo_crnn.cpp
+++ ncnn_demo_crnn.cpp
@@ -3,8 +3,6 @@
 #include "net.h"
 #include <iostream>
 using namespace std;
-//"#京沪津渝冀晋蒙辽吉黑苏浙皖闽赣鲁豫鄂湘粤桂琼川贵云藏陕甘青宁新学警港澳挂使领民航深0123456789ABCDEFGHJKLMNPQRSTUVWXYZ"
-
 string plate_chars[76] = { "#","京", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "吉", "黑",
                            "苏", "浙", "皖", "闽", "赣", "鲁", "豫", "鄂", "湘", "粤",
                            "桂", "琼", "川", "贵", "云", "藏", "陕", "甘", "青", "宁",
@@ -15,27 +13,6 @@ string plate_chars[76] = { "#","京", "沪", "津", "渝", "冀", "晋", "蒙", "辽", "
                            "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V",
                            "W", "X", "Y", "Z"};
 
-//这个函数是官方提供的用于打印输出的tensor
-void pretty_print(const ncnn::Mat & m)
-{
-    for (int q = 0; q < m.c; q++)
-    {
-        const float* ptr = m.channel(q);
-        for (int y = 0; y < m.h; y++)
-        {
-            for (int x = 0; x < m.w; x++)
-            {
-                printf("%f ", ptr[x]);
-                //cout << "1111:" << ptr[x];
-            }
-            ptr += m.w;
-            printf("\n");
-        }
-        printf("------------------------\n");
-    }
-}
-
-
 int main()
 /*!
  *
@@ -44,52 +21,39 @@ int main()
 {
     //定义模型的网络
     ncnn::Net net;
-    //加载模型
-    //net.load_param("clf-sim.param");
-    //net.load_model("clf-sim.bin");
 
+    //加载模型
     net.load_param("crnn.param");
-    //net.load_param("lpr2d-sim.param");
     net.load_model("crnn.bin");
-    //net.load_model("lpr2d-sim.bin");
 
     //使用opencv以灰度图读取图片
-    //cv::Mat img = cv::imread("C:/Users/Administrator/Desktop/test.jpg");
     cv::Mat img = cv::imread("test.jpg");
+
     //获取图片的宽
     int w = img.cols;
+
     //获取图片的高
     int h = img.rows;
-//    cout << w << endl << h<<endl;
-    cv::imshow("aa", img);
+
+//    显示图片
+    cv::imshow("img", img);
     cv::waitKey(0);
 
-    //将OpenCV的图片转为ncnn格式的图片,并且将图片缩放到224×224之间
-    //ncnn::Mat in = ncnn::Mat::from_pixels_resize(img.data, ncnn::Mat::PIXEL_GRAY, w, h, 24, 94);
-    //ncnn::Mat in = ncnn::Mat::from_pixels_resize(img.data, ncnn::Mat::PIXEL_BGR2RGB, w, h, 94, 24);
+    //将OpenCV的图片转为ncnn格式的图片,并且将图片resize
     ncnn::Mat in = ncnn::Mat::from_pixels_resize(img.data, ncnn::Mat::PIXEL_BGR, w, h, 168, 48);
-//    0.588, 0.193
-//    pretty_print(in);
+
     float mean[3] = { 149.94, 149.94, 149.94 };
     float norm[3] = { 0.020319,0.020319,0.020319 };
-    //对图片进行归一化,将像素归一化到-1~1之间
+    //对图片进行归一化,将像素归一化到-1~1之间后，再将
     in.substract_mean_normalize(mean, norm);
 
     ncnn::Extractor ex = net.create_extractor();
     ex.set_light_mode(true);
     //设置线程个数
     ex.set_num_threads(1);
-//    cout << in.c << endl;
-//    cout << in.h << endl;
-//    cout << in.w << endl;
-    //cout << in.d << endl;
-    //将图片放入到网络中,进行前向推理
-    //ex.input("input.1", in);
-    pretty_print(in);
     ex.input("input.1", in);
-//    cout << "输入1:" << in.channel(0)[1] << endl;
     ncnn::Mat feat;
-    ncnn::Mat feat0;
+
     //获取网络的输出结果
     ex.extract("108", feat);
 
@@ -106,14 +70,11 @@ int main()
             float preb[76];
             for (int y = 0; y < m.h; y++)  //遍历68个字符串位置
             {
-                //printf("%f ", ptr[x]);
                 preb[y] = ptr[x];  //将18个
                 ptr += m.w;
             }
             int max_num_index = max_element(preb, preb + 68) - preb;
-            //cout << max_num_index << endl;
             prebs[x] = max_num_index;
-            //printf("------------------------\n");
         }
 
         //去重复、去空白
@@ -137,15 +98,14 @@ int main()
         // 下面进行车牌lable按照字典进行转化为字符串
         string no_repeat_blank_c = "";
         for (int hh : no_repeat_blank_label) {
-            //cout<<"hh:" << hh << endl;
             no_repeat_blank_c += plate_chars[hh];
         }
         cout << "单个车牌:" << no_repeat_blank_c << endl;
 
         final_plate_str.push_back(no_repeat_blank_c);
-        for (string hhh : final_plate_str) {
-            cout << "所有车牌:" << hhh << endl;
-            finale_plate += hhh;
+        for (string plate_char : final_plate_str) {
+            cout << "所有车牌:" << plate_char << endl;
+            finale_plate += plate_char;
         }
     }
     string str = finale_plate;
